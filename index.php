44b142ab18755466189e5d434192e4330a823ba0
Chilled Developer <68301444+TheChilledDev@users.noreply.github.com>
2020-07-14 21:43:50 +0100

    Added Fonts and Text Sizes
    The fonts added in this update are Sans Serif, Serif, and Monospaced. Use them with the following class names...
    
    softwayr-sans-serif
    softwayr-serif
    softwayr-monospaced
    
    For text sizes, use the following classes...
    
    softwayr-small
    softwayr-normal
    softwayr-large
    softwayr-xlarge
    softwayr-xxlarge
    softwayr-xxxlarge
    softwayr-jumbo

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<title>Softwayr CSS</title>
+		
+		<link rel="stylesheet" type="text/css" href="softwayr.css?version=<?php echo filemtime('softwayr.css'); ?>">
+	</head>
+	
+	<body class="softwayr-sans-serif">
+		<h1>Softwayr CSS</h1>
+		<p>Welcome to Softwayr CSS!</p>
+		<hr>
+		<p class="softwayr-small">Small Test</p>
+		<p class="softwayr-normal">Normal Text</p>
+		<p class="softwayr-large">Large Text</p>
+		<p class="softwayr-xlarge">Extra Large Text</p>
+		<p class="softwayr-xxlarge">Extra Extra Large Text</p>
+		<p class="softwayr-xxxlarge">Extra Extra Extra Large Text</p>
+		<p class="softwayr-jumbo">Jumbo Text</p>
+	</body>
+</html>
